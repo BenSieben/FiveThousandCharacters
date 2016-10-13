afb6b057d46087315c7366b6f55acb55892c428e
Ben <waripod@hotmail.com>
2016-10-12 18:01:01 -0700

    added favicon for the website

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Homework 3</title>
-  <meta charset="utf-8" />
+    <title>Homework 3</title>
+    <meta charset="utf-8" />
+    <link rel="icon" href="src/resources/favicon.ico" />
 </head>
 <body>
-  <p>Initial commit!</p>
+    <p>Initial commit!</p>
 </body>
 </html>
