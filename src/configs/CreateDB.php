f880272dfff67af9731be92c35ca294e1ca18939
Ben <waripod@hotmail.com>
2016-10-21 14:14:17 -0700

    Switched CreateDB.php to use object-oriented mysqli

--- src/configs/CreateDB.php
+++ src/configs/CreateDB.php
@@ -10,8 +10,8 @@ require_once("Config.php");
  */
 
 // first attempt to establish a connection to the database
-$db = mysqli_connect(Config::DB_HOST, Config::DB_USERNAME, Config::DB_PASSWORD, "", Config::DB_PORT);
-if(!$db) {
+$mysqli = new mysqli(Config::DB_HOST, Config::DB_USERNAME, Config::DB_PASSWORD, "", Config::DB_PORT);
+if($mysqli->connect_errno) {
     echo("Error connecting to \"" . Config::DB_HOST . "\" with username \"" . Config::DB_USERNAME
         . "\", password \"" . Config::DB_PASSWORD . "\", and port \"" . Config::DB_PORT . "\".");
     return;
@@ -22,13 +22,13 @@ echo("Connection success, now setting up database " . Config::DB_DATABASE . " (a
     "this name will be deleted completely, and some initial sample data will be loaded into the database)\n");
 
 // create the database and use it
-mysqli_query($db, "DROP DATABASE IF EXISTS " . Config::DB_DATABASE);
-mysqli_query($db, "CREATE DATABASE " . Config::DB_DATABASE);
-mysqli_query($db, "USE " . Config::DB_DATABASE);
+$mysqli->query("DROP DATABASE IF EXISTS " . Config::DB_DATABASE);
+$mysqli->query("CREATE DATABASE " . Config::DB_DATABASE);
+$mysqli->query("USE " . Config::DB_DATABASE);
 
 // create the Story relation, which contains most information for each written story
-mysqli_query($db, "DROP TABLE IF EXISTS Story");
-mysqli_query($db, "CREATE TABLE Story(sID VARCHAR(" . Config::WS_MAX_IDENTIFIER_LENGTH . "), " .
+$mysqli->query("DROP TABLE IF EXISTS Story");
+$mysqli->query("CREATE TABLE Story(sID VARCHAR(" . Config::WS_MAX_IDENTIFIER_LENGTH . "), " .
                                      "title VARCHAR(" . Config::WS_MAX_TITLE_LENGTH . ") NOT NULL, " .
                                      "author VARCHAR(" . Config::WS_MAX_AUTHOR_LENGTH . ") NOT NULL, " .
                                      "submitTime DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, " .
@@ -39,21 +39,21 @@ mysqli_query($db, "CREATE TABLE Story(sID VARCHAR(" . Config::WS_MAX_IDENTIFIER_
                                      "PRIMARY KEY (sID))");
 
 // create the Genre relation, which contains a list of possible genres that can be associated with stories
-mysqli_query($db, "DROP TABLE IF EXISTS Genre");
-mysqli_query($db, "CREATE TABLE Genre(gID INT AUTO_INCREMENT, " .
+$mysqli->query("DROP TABLE IF EXISTS Genre");
+$mysqli->query("CREATE TABLE Genre(gID INT AUTO_INCREMENT, " .
                                      "name VARCHAR(" . Config::MAX_GENRE_NAME_LENGTH . ") NOT NULL, " .
                                      "PRIMARY KEY (gID))");
 
 // create the StoryGenres relation, which contains a list of story ID's and genre ID's to tell the genres of stories
-mysqli_query($db, "DROP TABLE IF EXISTS StoryGenres");
-mysqli_query($db, "CREATE TABLE StoryGenres(sID VARCHAR(" . Config::WS_MAX_IDENTIFIER_LENGTH . "), " .
+$mysqli->query("DROP TABLE IF EXISTS StoryGenres");
+$mysqli->query("CREATE TABLE StoryGenres(sID VARCHAR(" . Config::WS_MAX_IDENTIFIER_LENGTH . "), " .
                                           "gID INT, " .
                                           "PRIMARY KEY (sID, gID), " .
                                           "FOREIGN KEY (sID) REFERENCES Story(sID), " .
                                           "FOREIGN KEY (gID) REFERENCES Genre(gID))");
 
 // load up all possible genres into the Genre relation
-mysqli_query($db, "INSERT INTO Genre(name) VALUES('Action'), " .
+$mysqli->query("INSERT INTO Genre(name) VALUES('Action'), " .
                                                 "('Adventure'), " .
                                                 "('Comedy'), " .
                                                 "('Crime'), " .
@@ -70,7 +70,7 @@ mysqli_query($db, "INSERT INTO Genre(name) VALUES('Action'), " .
                                                 "('Western')");
 
 // load up some sample stories
-mysqli_query($db, "INSERT INTO Story(sID, title, author, submitTime, content) " .
+$mysqli->query("INSERT INTO Story(sID, title, author, submitTime, content) " .
     "VALUES('sample01', 'Winter Frost', 'Ace Brown', '2000-01-01 01:30:26', 'Winter is cold.\n\n I feel frosty...'), " .
           "('sample02', 'Summer Blaze', 'Carl Dover', '2001-07-20 16:22:00', 'The day is late, and the night is bright.'), " .
           "('sample03', 'Duck Drive', 'Edgar Funk', '2002-04-20 12:38:48', 'Funky fresh!'), " .
@@ -83,7 +83,7 @@ mysqli_query($db, "INSERT INTO Story(sID, title, author, submitTime, content) " 
           "('sample10', 'Pleasentries', 'Tess Uloo', '2009-11-16 20:16:09', 'It is pleasant to do nothing.\n\nIt is pleasant to shop.\n\nDon\'t forget about watching television, either!')");
 
 // associate some genres with the sample stories
-mysqli_query($db, "INSERT INTO StoryGenres(sID, gID)" .
+$mysqli->query("INSERT INTO StoryGenres(sID, gID)" .
     "VALUES('sample01', 10), " .
           "('sample01', 1), " .
           "('sample02', 5), " .
@@ -100,7 +100,7 @@ mysqli_query($db, "INSERT INTO StoryGenres(sID, gID)" .
           "('sample09', 7), " .
           "('sample10', 11)");
 
-mysqli_close($db);
+$mysqli->close();
 
 // let user know process has finished successfully
 echo("Done! The database should be ready for use now\n");
